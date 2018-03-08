b04824063e78ff400a4b25f0b8ee17d8bec2372a
alexis <a.flores@cathedralsw.com>
2018-03-08 12:19:30 +0100

    changes ERROR

--- ByeWorld.java
+++ ByeWorld.java
@@ -1,5 +1,5 @@
 public class ByeWorld {
-   public static void main(String[] args) {
+   public static void .main(String[] args) {
       // Prints "Bye, World" in the terminal window.
       System.out.println("Bye, World!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
    }
